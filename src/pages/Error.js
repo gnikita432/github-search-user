a744344f3d0ef79fefd638b8d99e9078472c4b84
Nikita Anil Gupta <gnikita432@gmail.com>
2021-05-20 13:17:01 +0530

    implemented Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,16 @@
 import React from "react";
-
+import "./Error.css";
+import { Link } from "react-router-dom";
 function Error() {
   return (
     <div className="error">
-      <h1>Error page</h1>
+      <h1 className="error__heading">Error 404</h1>
+      <h3 className="error__message">
+        Sorry, the page you tried cannot be found
+      </h3>
+      <Link to="/">
+        <button className="error__button">back home</button>
+      </Link>
     </div>
   );
 }
