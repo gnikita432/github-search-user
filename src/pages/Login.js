bda18d77ccaa835892d3211f6914a49cbdb9c07d
Nikita Anil Gupta <gnikita432@gmail.com>
2021-05-20 14:28:33 +0530

    Implemented Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import "./Login.css";
+import loginImg from "../images/login-image.svg";
+import { Link } from "react-router-dom";
 function Login() {
   return (
     <div className="login">
-      <h1>Login page</h1>
+      <img className="login__img" src={loginImg} alt="Github user" />
+      <h1 className="login__heading">Github user</h1>
+      <Link to="/">
+        <button className="login__button">Login</button>
+      </Link>
     </div>
   );
 }
